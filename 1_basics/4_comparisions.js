10a1fdbffba8ac7868e0039bcc705937489b4d5f
ABHAYRAJ SINGH ARYA <abhayrajsingharya@gmail.com>
2025-04-02 13:21:13 +0000

    Done

---
+++ 1_basics/4_comparisions.js
@@ -0,0 +1,20 @@
+console.log(2 >1);
+console.log(2 >=1);
+
+console.log("2" > 1);
+
+console.log(null > 0);
+
+console.log(undefined == 0);
+
+// strict check
+console.log("2" === 2);
+
+/*
+Avoid comparisions when you have different data type to compare
+*/
+
+
+
+
+
