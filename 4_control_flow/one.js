cde75a38ed04011b2a3531080ee29a3abe76fb38
ABHAYRAJ SINGH ARYA <abhayrajsingharya@gmail.com>
2025-04-05 10:24:39 +0000

    if_else

---
+++ 4_control_flow/one.js
@@ -0,0 +1,39 @@
+// if statement
+
+
+
+// comparision operator
+
+{/* < less than
+> greater than 
+== equal
+<= less than equal
+>= greater than eaual
+!= not eaual 
+=== this also check the type of variable
+*/}
+
+// const temperature = 50
+// if(temperature < 50){
+//     console.log(`Temperature less than 50`);
+    
+// }else{
+//     console.log(`Temperature greater than or equal 50`);
+    
+// }
+
+
+
+
+// nested if else
+const balance = 200
+if(balance < 500){
+    console.log(`balance is less than 500`);
+    
+}else if(balance > 500){
+    console.log(`balance is greater than 500`);
+    
+}else{
+    console.log(`balance is equal to 500`);
+    
+}
